c978c58db61e1d93dad6925b043ab84a2c8ba5ec
Diogo S. Florêncio <diogosflorencio@gmail.com>
2023-03-08 13:01:57 -0300

    cos, sin, tan e radians

--- ex018.py
+++ ex018.py
@@ -1,3 +1,5 @@
+#cos, sin, tan, radians
+
 from math import cos, sin, tan, radians
 angulo = float(input('Digite um angulo para saber seu seno e consseno: '))
 print(f'seu cosseno é {cos(radians(angulo)):.2f} e seu seno é {sin(radians(angulo)):.2f} e a tangente é {tan(radians(angulo)):.2f}')
