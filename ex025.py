409316f26046bfa706db09203031e8e3ec49c1d4
Diogo S. Florêncio <diogosflorencio@gmail.com>
2023-03-10 08:50:57 -0300

    tem silva no nome?

--- ex025.py
+++ ex025.py
@@ -1,11 +1,11 @@
 #criar um programa que leia o nome de uma pessoa e mostre se há ou n SILVA no nome
-nome = str(input('digite seu nome para saber se vc é pobre: '))
+nome = str(input('digite seu nome para saber se vc é pobre igual a mim: '))
 nome = nome.upper()
 #nome = nome.find('SILVA')
 #if nome == -1:
-#    print('comemore, mesmo que vc seja pobre por enquanto,seu futuro será maravilho. seu nome tem peso KASJDKJSD')
+#    print('não tem silva')
 #else:
-#        print('sim, pobre, vc tem silva no seu nome. F =(')
+#        print('sim, pobre, vc tem silva no seu nome. F =( (tbm junto)')
 
 
 # ou faz assim:
