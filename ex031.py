61aed88f1c4044987547200104b6951faa688b62
Diogo S. Florêncio <diogosflorencio@gmail.com>
2023-03-13 13:11:09 -0300

    custo por distancia

--- ex031.py
+++ ex031.py
@@ -1,7 +1,11 @@
+#custo/distancia
+
 distancia = float(input('qual será a distancia? '))
+
 '''if distancia <= 200:
     print(f'sua viagem custará R$ {distancia*0.5}')
 else:
     print(f'sua viagem custará {distancia*0.45}')'''
+
 print(distancia*0.5 if distancia <= 200 else distancia*0.45)
 
