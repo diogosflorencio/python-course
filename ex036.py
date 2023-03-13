a33861658fdf4e8e57d55fb50b8727c8825e8b00
Diogo S. Florêncio <diogosflorencio@gmail.com>
2023-03-13 13:23:43 -0300

    emprestimo bancario

--- ex036.py
+++ ex036.py
@@ -1,17 +1,6 @@
-'''import math
-cor = '\033[1;31;40m'
-coroff = '\033[m'
-print(f'{cor}Olá, Mundo!!!{coroff}')
+#emprestimo bancario
 
-nome = str(input('Digite o nome da marca do seu carro: '))
-if nome == 'gol':
-    print(f'seu carrol gol é bem popular!')
-elif nome in 'porche mustangue ferrari lamborguini':
-    print('caralho, o cara é rico vey slc!')
-elif nome == 'fusca':
-    print('kkkkkkkkkk, mas de boas, adoraria ter um tbm')
-else:
-    print('c n tem carro?! eu tbm n, mas jaja nós teremos garai!')'''
+#n sei o que foi isso. 
 
 print('Calculadora de emprestimo bancario!')
 valorcasa = float(input('Qual o valor da casa que vc quer comprar? '))
@@ -24,3 +13,4 @@ if valorcasamensal > trintaporcem:
     print(f'Infelizmente o valor das parcelas da casa ({valorcasamensal}) excede 30% do salário ({trintaporcem}) ')
 elif valorcasamensal <= trintaporcem:
     print(f'seu emprestimo está garantido, vc pagará {valorcasamensal} durante {quantidademeses} e isso consumirá {(valorcasamensal/100)*salario}')
+        
